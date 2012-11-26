d069f6ce2ca36a1d8362af4dfc2554bbd40d11e3
Pierre Vaidie <pierre.vaidie@gmail.com>
2012-11-26 16:56:02 +0100

    Add install and config files

---
+++ src/FineAdmin.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Copyright (c) 2012 David Negrier
+ * 
+ * See the file LICENSE.txt for copying permission.
+ */
+
+
+MoufUtils::registerMainMenu('htmlMainMenu', 'HTML', null, 'mainMenu', 40);
+MoufUtils::registerMenuItem('htmlFineMainMenu', 'Fine', null, 'htmlMainMenu', 10);
+MoufUtils::registerMenuItem('htmlFineEditTranslationMenuItem', 'Edit translations', 'mouf/editLabels/missinglabels?name=translationService', 'htmlFineMainMenu', 10);
+MoufUtils::registerMenuItem('htmlFineEditTranslationAnyInstanceMenuItem', 'Edit translations (any instance)', 'javascript:chooseInstancePopup("FinePHPArrayTranslationService", "'.ROOT_URL.'mouf/editLabels/missinglabels?name=", "'.ROOT_URL.'")', 'htmlFineMainMenu', 20);
+MoufUtils::registerMenuItem('htmlFineSupportedLanguagesMenuItem', 'Supported languages', 'javascript:chooseInstancePopup("FinePHPArrayTranslationService", "'.ROOT_URL.'mouf/editLabels/supportedLanguages?name=", "'.ROOT_URL.'")', 'htmlFineMainMenu', 30);
+MoufUtils::registerMenuItem('htmlFineEnableDisable2MenuItem', 'Enable/Disable translation', 'mouf/editLabels/', 'htmlFineMainMenu', 40);
+MoufUtils::registerMenuItem('htmlFineImportCSV2MenuItem', 'Import/Export', 'javascript:chooseInstancePopup("FinePHPArrayTranslationService", "'.ROOT_URL.'mouf/editLabels/excelimport?name=", "'.ROOT_URL.'")', 'htmlFineMainMenu', 50);
+
+// Controller declaration
+MoufManager::getMoufManager()->declareComponent('editLabels', 'EditLabelController', true);
+MoufManager::getMoufManager()->bindComponents('editLabels', 'template', 'moufTemplate');
+?>
---
+++ src/install.php
@@ -0,0 +1,47 @@
+<?php
+use Mouf\MoufManager;
+use Mouf\Actions\InstallUtils;
+
+// First, let's request the install utilities
+
+
+// Let's init Mouf
+
+InstallUtils::init(InstallUtils::$INIT_APP);
+
+// Let's create the instance
+$moufManager = MoufManager::getMoufManager();
+
+//language detection service
+if ($moufManager->instanceExists("defaultLanguageDetection")) {
+	$defaultLanguageDetection = $moufManager->getInstanceDescriptor("defaultLanguageDetection");
+} else {
+	$defaultLanguageDetection = $moufManager->createInstance("BrowserLanguageDetection");
+	$defaultLanguageDetection->setName("defaultLanguageDetection");
+}
+
+//common translation service
+if ($moufManager->instanceExists("fineCommonTranslationService")) {
+	$fineCommonTranslationService = $moufManager->getInstanceDescriptor("fineCommonTranslationService");
+} else {
+	$fineCommonTranslationService = $moufManager->createInstance("FinePHPArrayTranslationService");
+	$fineCommonTranslationService->setName("fineCommonTranslationService");
+	$fineCommonTranslationService->getProperty("languageDetection")->setValue($defaultLanguageDetection);
+}
+$fineCommonTranslationService->getProperty("i18nMessagePath")->setValue("vendor/mouf/utils.i18n.fine/resources/");
+
+//defaultTranslationService
+if ($moufManager->instanceExists("defaultTranslationService")) {
+	$defaultTranslationService = $moufManager->getInstanceDescriptor("defaultTranslationService");
+} else {
+	$defaultTranslationService = $moufManager->createInstance("FinePHPArrayTranslationService");
+	$defaultTranslationService->setName("defaultTranslationService");
+	$defaultTranslationService->getProperty("i18nMessagePath")->setValue("resources/");
+	$defaultTranslationService->getProperty("languageDetection")->setValue($defaultLanguageDetection);
+}
+
+// Let's rewrite the MoufComponents.php file to save the component
+$moufManager->rewriteMouf();
+
+// Finally, let's continue the install
+InstallUtils::continueInstall();
