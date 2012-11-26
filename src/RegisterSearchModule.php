6ab565a367ea2953c96fcadda097ecd48c24fe44
Pierre Vaidie <pierre.vaidie@gmail.com>
2012-11-26 16:50:07 +0100

    Move install files (2)

---
+++ src/RegisterSearchModule.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Copyright (c) 2012 David Negrier
+ * 
+ * See the file LICENSE.txt for copying permission.
+ */
+
+MoufAdmin::getSearchService()->searchableServices[] = MoufManager::getMoufManager()->getInstance('editLabels');
